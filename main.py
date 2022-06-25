d679176c003ef87f859beada5b698d82084d94d5
BriChavez <brianachavez@gmail.com>
2022-06-24 18:41:14 -0700

    readjust code for prettiness and add function to change the popularity column to a float

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from pyspark.sql import SparkSession
 # make an instance of a SparkSession called 'spark'
 spark = SparkSession.builder.master('local').getOrCreate()
 
+# create a custom schema to ensure data types and column names
 custom_schema = StructType([
     StructField('index', IntegerType()),
     StructField('artist_popularity', LongType()),
@@ -20,24 +21,24 @@ custom_schema = StructType([
     StructField('type', StringType())])
 
 """Extract Information"""
+# create a spark dataframa from a csv with our custom schema
 spark_df = (spark.read.format("csv"
-                              ).options(header="true"
-                                        ).schema(custom_schema
-                                                 ).load("data/spotify_artists.csv"))
+                        ).options(header="true"
+                        ).schema(custom_schema
+                        ).load("data/spotify_artists.csv"))
 
 
 """Profile the Data:"""
-
 # Show a description (summary) of the Spark DataFrame.
 spark_df.describe
 # Print the schema of the DataFrame.
 spark_df.printSchema()
 # Select and show just the first 10 values in the 'name' and 'genres' columns.
-spark_df.select('name', 'genres').show(8)
+spark_df.select('name', 'genres').show(10)
 # Where the genre is an empty list, replace it with ['elevator music'].
 spark_df = spark_df.withColumn('genres', regexp_replace(
                                 'genres', r"\[\]", "['elevator music']"))
-spark_df.select('name', 'genres').show(8)
+spark_df.select('name', 'genres').show(10)
 # For the columns 'artist_popularity' and 'followers', cast the data type as integers.
 # done in the schema, but ill throw in the code, so you know i know it
 spark_df = spark_df.withColumn('artist_popularity', col(
@@ -45,33 +46,28 @@ spark_df = spark_df.withColumn('artist_popularity', col(
 spark_df = spark_df.withColumn('followers', col(
                     'followers').cast(IntegerType()))
 # Sort the data in descending order by number of followers.
-spark_df.select('*').sort('followers', ascending=False).show(5)
+spark_df.select('*').sort('followers', ascending=False).show(10)
+
 
 """process to change the artist popularity to percent and rename the column"""
-
 # user defined function to divide x by 100
 pop_contest = udf(lambda x: x/100)
 # apply our udf to the column artist popularity
-pop_contest_df = spark_df.withColumn('artist_popularity',
-                                     pop_contest(spark_df['artist_popularity']))
+pop_contest_df = spark_df.withColumn('artist_popularity', 
+                pop_contest(spark_df['artist_popularity']))
 # Rename the column 'popularity_percent'.
-pop_contest_df = spark_df.withColumnRenamed(
-    'artist_popularity', 'popularity_percent')
-# print the least cool kids to chow our function is working
-pop_contest_df.select('popularity_percent', 'followers',
-                      'name', 'genres').sort('popularity_percent').show(8)
+pop_contest_df = spark_df.withColumnRenamed('artist_popularity', 
+                                            'popularity_percent')
+# change datatype of the percent column from a string to a float
+pop_contest_df = pop_contest_df.withColumn('popularity_percent', 
+                            pop_contest_df['popularity_percent'
+                                    ].cast('float'))
 
 """Extract Information"""
-
 # Group the data by artist popularity, and show the count for each group.
-spark_df = spark_df.withColumn('artist_popularity', spark_df[
-    'artist_popularity'].cast('double'))
-spark_df.printSchema()
-
-spark_df.groupBy('artist_popularity').sum('artist_popularity').show(8)
+spark_df.groupBy('artist_popularity').sum('artist_popularity').show(10)
 
 """save dataframe as a parquet"""
-
 # write the code to save the DataFrame as a Parquet file in the /data directory.
 spark_df.write.parquet('data/spotify.parquet')
 
